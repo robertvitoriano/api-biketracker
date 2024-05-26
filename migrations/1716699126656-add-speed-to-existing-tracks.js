15eff5b2c528957996593b29b5679d322f04dfe0
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-05-26 14:38:33 +0900

    set up migrations with dynamic path, since build folder is used

--- migrations/1716699126656-add-speed-to-existing-tracks.js
+++ migrations/1716699126656-add-speed-to-existing-tracks.js
@@ -1,7 +1,9 @@
-const {Track} = require("/media/robert/storage9/Central/projetos/backend/javascript/node/biketracker-api/build/src/database/connection.js");
+
+const {Track} = require(`${__filename.split('/migrations/')[0]}/build/src/database/connection.js`);
 
 async function up() {
   const alltracks = await Track.find({});
+  
   for (const track of alltracks) {
     track.speed = track.distance / (track.time / 3600);
     await track.save();
@@ -12,7 +14,8 @@ async function up() {
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 async function down() {
-  const alltracks = Track.find({});
+  const alltracks = await Track.find({});
+  
   for (const track of alltracks) {
     track.speed = "";
     track.save();
